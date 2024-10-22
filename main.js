1c07a5e91b3b1b272d72018b974b9668e3d130dc
kreit <reitenbaughkm@gmail.com>
2024-10-22 09:55:29 -0500

    On branch master  Changes to be committed: 	modified:   main.js

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 /******************************/
 //Your Code Here 
-
+const  = function(day){
+  case "1":{
+    console.log("Monday")
+      break
+  } case "2":{
+    console.log(Tuesday)
+      break
+  }
+  }
+}
 
 
 
